14e6738cff1d36e6c55e4b6ba108bb9f1e1d54a9
Sergey Efimov <sergeyefimov234@gmail.com>
2022-05-26 12:07:04 +0300

    Init commit

--- src/test/java/ru/netology/org/SQRServiceTest.java
+++ src/test/java/ru/netology/org/SQRServiceTest.java
@@ -2,7 +2,7 @@ package ru.netology.org;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SQRServiceTest {
 
